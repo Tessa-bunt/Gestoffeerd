536d143b7f5c2c4f36c7885500d4fb37df7ccf8e
leonie <leonievdb1@outlook.com>
2026-03-11 10:13:46 +0100

    footer en nav

---
+++ static/footer.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>footer</title>
+    <link rel="stylesheet" href="../style/footer.css">
+    <link rel="stylesheet" href="../style/standardCSS.css">
+</head>
+
+<body>
+    <footer>
+        <div class="footerOptions">
+            <div class="products">
+                <h1>Producten</h1>
+                <div class="optionVloer">
+                    <a href="">PCV vloer</a>
+                </div>
+                <div class="optionVloer">
+                    <a href="">Vloerbedekking</a>
+                </div>
+                <div class="optionVloer">
+                    <a href="">Houten vloer</a>
+                </div>
+                <div class="optionVloer">
+                    <a href="">Laminaat</a>
+                </div>
+            </div>
+
+            <div class="info">
+                <div class="locatie">
+                    <h1>Onze locatie</h1>
+                    <p>De Dijken 7C 1747 EE Tuitjenhorn.</p>
+                </div>
+                <div class="tijden">
+                    <h1>Showroom uren</h1>
+                    <p>ma / vr 09:00 tot 18:00</p>
+                </div>
+
+            </div>
+        </div>
+
+    </footer>
+
+</body>
+
+</html>
---
+++ static/nav.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nav</title>
+    <link rel="stylesheet" href="../style/nav.css">
+    <link rel="stylesheet" href="../style/standardCSS.css">
+</head>
+<body>
+    <div class="topNav">
+        <div class="navInfo">
+            <img src="../images/locatieIcon.png">
+            <p>De Dijken 7C 1747 EE Tuitjenhorn.</p>
+        </div>
+        <div class="navInfo">
+            <img src="../images/phoneIcon.png">
+            <p>Bel: (0224) 850 655</p>
+        </div>
+        <div class="navInfo">
+            <img src="../images/timeIcon.png">
+            <p>ma / vr 09:00 tot 18:00</p>
+        </div>
+    </div>
+
+    <div class="bottomNav">
+        <div class="logo">
+            <img src="../images/logo.png">
+        </div>
+        <div class="navOptions">
+            <div class="option">
+                <a href="home.php">Home</a>
+            </div>
+            <div class="option">
+                <a href="about.php">Wie zijn wij</a>
+            </div>
+            <div class="option">
+                <a href="products.php">Products</a>
+            </div>
+            <div class="option">
+                <a href="onderhoud.php">Onderhoud</s>
+            </div>
+            <div class="option">
+                <a href="referenties.php">Referenties</a>
+            </div>
+            <div class="option">
+                <a href="projecten.php">Projecten</a>
+            </div>
+            <div class="option">
+                <a href="contact.php">Contact ons</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
