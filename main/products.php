f79eff47bda43f0d3950da00065806ae3cbb8cd3
leonie <leonievdb1@outlook.com>
2026-03-13 11:51:30 +0100

    referenties

--- main/products.php
+++ main/products.php
@@ -1,11 +1,62 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="../style/standardCSS.css">
+    <link rel="stylesheet" href="../style/home.css">
+    <link rel="stylesheet" href="../style/products.css">
 </head>
+
 <body>
     <?php include '../static/nav.php' ?>
+
+    <div class="header">
+        <div class="start">
+            <div class="startInfo">
+                <h1>PRODUCTEN</h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="floors">
+        <div class="allFloorOptions">
+            <div class="floorOption">
+                <a href="">
+                    <h2>Houten Vloeren</h2>
+                </a>
+            </div>
+            <div class="floorOption">
+                <a href="">
+                    <h2>Laminaat</h2>
+                </a>
+            </div>
+            <div class="floorOption">
+                <a href="">
+                    <h2>Harde Vloerbedekking</h2>
+                </a>
+            </div>
+            <div class="floorOption">
+                <a href="">
+                    <h2>PVC Vloeren</h2>
+                </a>
+            </div>
+              <div class="floorOption">
+                <a href="">
+                    <h2>Zachte vloerbedekking</h2>
+                </a>
+            </div>
+              <div class="floorOption">
+                <a href="">
+                    <h2>Parket</h2>
+                </a>
+            </div>
+        </div>
+
+
+    <?php include '../static/footer.php' ?>
 </body>
+
 </html>
--- main/referenties.php
+++ main/referenties.php
@@ -1,11 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="../style/standardCSS.css">
+    <link rel="stylesheet" href="../style/home.css">
+    <link rel="stylesheet" href="../style/about.css">
 </head>
+
 <body>
     <?php include '../static/nav.php' ?>
+
+    <div class="header">
+        <div class="start">
+            <div class="startInfo">
+                <h1>REFERENTIES</h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="referenties">
+        <div class="referentie">
+            <h1>Referenties</h1>
+            <p>"Friso en Collega bij deze nogmaals onze dank voor jullie geleverde werk en bedankt voor de zeer gezellige dag. Als we nog eens iets nodig hebben weten we jullie te vinden"
+                <br>Familie smit
+            </p>
+             <p>"Friso en Collega bij deze nogmaals onze dank voor jullie geleverde werk en bedankt voor de zeer gezellige dag. Als we nog eens iets nodig hebben weten we jullie te vinden"
+                <br>Familie smit
+            </p>
+             <p>"Friso en Collega bij deze nogmaals onze dank voor jullie geleverde werk en bedankt voor de zeer gezellige dag. Als we nog eens iets nodig hebben weten we jullie te vinden"
+                <br>Familie smit
+            </p>
+             <p>"Friso en Collega bij deze nogmaals onze dank voor jullie geleverde werk en bedankt voor de zeer gezellige dag. Als we nog eens iets nodig hebben weten we jullie te vinden"
+                <br>Familie smit
+            </p>
+             <p>"Friso en Collega bij deze nogmaals onze dank voor jullie geleverde werk en bedankt voor de zeer gezellige dag. Als we nog eens iets nodig hebben weten we jullie te vinden"
+                <br>Familie smit
+            </p>
+             <p>"Friso en Collega bij deze nogmaals onze dank voor jullie geleverde werk en bedankt voor de zeer gezellige dag. Als we nog eens iets nodig hebben weten we jullie te vinden"
+                <br>Familie smit
+            </p>
+        </div>
+
+    </div>
+
+
+    <?php include '../static/footer.php' ?>
 </body>
+
 </html>
